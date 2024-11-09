f8243e6a53284d4e7322f34f04bfd8a41e01286d
TunaSandwinch <winch.mascarenas@gmail.com>
2024-11-09 20:52:49 +0800

    feat: most registered hours can now be displayed

--- lib/event_manager_assigment.rb
+++ lib/event_manager_assigment.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'date'
+require 'time'
 puts 'EventManager initialized.'
 
 contents = CSV.open(
@@ -8,23 +9,44 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
-def fetch_hours(date)
+def hours(date)
   hours = []
   date.each do |row|
     hour = row[:regdate].split()
     hour.shift
     hour = hour.join.split('')
-    hour.length == 5 ? hours << "#{hour[0, 2].join}:00" : hours << "#{hour[0, 1].join}:00"
+    hour.length == 5 ? hours << "#{hour[0, 2].join}:00" : hours << "#{hour[0, 1].join.rjust(2, '0')}:00"
   end
   hours
 end
 
-hours = fetch_hours(contents)
-hours.each do |hour|
-  p "#{hour}: #{hours.count(hour)}"
+def hour_frequencies(hours)
+  hours.map {|hour| hours.count(hour)}.uniq.sort{|a, b| b <=> a} 
 end
 
+def hours_with_frequency(hours)
+  hours.reduce(Hash.new(0)) do |storage, item|
+    storage[item] += 1
+    storage
+  end
+end
 
+hours_array = hours(contents)
+def most_registered_hours(hour_freq, hour_with_freq)
+  array = []
+  hour_with_freq.each do |key, value|
+    array << key if value == hour_frequencies(hour_freq)[0]
+  end
+  array
+end
+
+def print_most_reg_hours(hours)
+  puts 'the most registered hours are:'
+  hours.each do |hour|
+    time_obj = Time.strptime(hour, '%H:%M')
+    p time_obj.strftime("%I:%M %p").strip
+  end
+end
 
 def clean_phone_numbers(number)
   plain_number = number.gsub(/[^\d]+/ , '')
@@ -40,6 +62,8 @@ end
 # contents.each do |row|
 #   puts clean_phone_numbers(row[:homephone])
 # end
+most_reg_hours = most_registered_hours(hour_frequencies(hours_array), hours_with_frequency(hours_array))
 
+print_most_reg_hours(most_reg_hours)
 
 
